90c224fb14e3e2faf7771b6541560ae5b96169e1
Soustin0502 <75117852+Soustin0502@users.noreply.github.com>
2025-06-09 22:02:56 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // ✅ use HashRouter
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/warp-the-computer-club/">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
