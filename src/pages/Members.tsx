1c310ad735998358dfb71f17f5c3efa053ed8120
Soustin0502 <75117852+Soustin0502@users.noreply.github.com>
2025-06-09 11:42:46 +0530

    Updated Members.tsx

--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -89,10 +89,11 @@ const Members = () => {
                   )}
                   {/*
                     MODIFIED GRADIENT CLASS:
-                    - Height changed from h-32 to h-48.
-                    - Gradient colors remain 'from-background to-transparent' as specified.
+                    - Height remains h-48.
+                    - Gradient colors changed to 'from-black via-black/70 to-transparent'
+                      to provide a stronger and bolder fade.
                   */}
-                  <div className="absolute bottom-0 left-0 right-0 h-48 bg-gradient-to-t from-background to-transparent z-10" />
+                  <div className="absolute bottom-0 left-0 right-0 h-48 bg-gradient-to-t from-black via-black/70 to-transparent z-10" />
                 </div>
                 <div className="relative z-20 p-4">
                   <CardHeader className="p-0 pb-2">
