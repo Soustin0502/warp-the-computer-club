d84759501eb77f16bc1a14a273b0885a6d7ab527
Soustin0502 <75117852+Soustin0502@users.noreply.github.com>
2025-06-09 11:33:24 +0530

    Updated Members.tsx

--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -87,7 +87,7 @@ const Members = () => {
                       </div>
                     </div>
                   )}
-                  <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent z-10" />
+                  <div className="absolute bottom-0 left-0 right-0 h-42 bg-gradient-to-t from-background to-transparent z-10" />
                 </div>
                 <div className="relative z-20 p-4">
                   <CardHeader className="p-0 pb-2">
