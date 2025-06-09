61701c0b85ef1074f471809528ae90348ba52c08
Soustin0502 <75117852+Soustin0502@users.noreply.github.com>
2025-06-09 11:30:30 +0530

    Updated Members.tsx

--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -87,13 +87,7 @@ const Members = () => {
                       </div>
                     </div>
                   )}
-                  {/*
-                    MODIFIED GRADIENT CLASS:
-                    - Increased height to h-48 for more coverage.
-                    - Changed gradient stops to go from solid black, through 80% black, to transparent.
-                      This creates a much bolder and more opaque fade effect from the image.
-                  */}
-                  <div className="absolute bottom-0 left-0 right-0 h-48 bg-gradient-to-t from-black via-black/80 to-transparent z-10" />
+                  <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent z-10" />
                 </div>
                 <div className="relative z-20 p-4">
                   <CardHeader className="p-0 pb-2">
