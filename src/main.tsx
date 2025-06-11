742f06bd4e8ae75a859a9bc746dc0a8e31c426a3
Soustin0502 <75117852+Soustin0502@users.noreply.github.com>
2025-06-11 10:41:12 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import { HashRouter } from 'react-router-dom'
+import { ThemeProvider } from './contexts/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </HashRouter>
   </React.StrictMode>,
+)
 )
