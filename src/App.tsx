35ab232100641639d1976b5568fcfa74c3e6204f
Soustin0502 <75117852+Soustin0502@users.noreply.github.com>
2025-06-11 11:40:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ function App() {
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </main>
-{/*             <Footer/> */}
+            <Footer/>
             <Toaster />
           </div>
         </AuthProvider>
